5f9a70aee22892d02a5ba6d1921be849b7179af7
Vanessa da Silva Machado <11640071652@MUZAMBINHO.EDU.BR>
2020-02-17 16:55:24 -0300

    Criando função básica.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+import { Container } from './styles';
+
+export default function App() {
+  
+  const [result, setResult] = useState();
+  const [expression, setExpression] = useState('');
+  
+  const point = c => {
+    let pc = 0;
+    
+    if(c === '*' || c === '/')pc = 2;
+    else if(c === '+' || c === '-') pc = 1;
+    else pc = 4;
+    
+    return pc;
+  }
+  
+  const handleSubmite = e => {
+    
+    e.preventDefault();
+    
+    var result = '';
+    var pilha = [];
+    
+    for(let k=0;k<expression.length;k++){
+      if(!isNaN(expression[k])) {
+        result += expression[k];
+      } else {
+
+        if(pilha.length() !== 0) pilha.push(expression[k]);
+        else if(point(pilha.top()) >=  point(expression[k])){
+          
+          let newCaracter = pilha.top();
+          pilha.pop();
+
+          result += newCaracter;
+          pilha.push(expression[k]);
+        } else result += expression[k];
+      }
+    }
+
+    let newCaracter = pilha.top();
+    pilha.pop();
+    result += newCaracter;
+    setResult(result);
+
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    <Container>
+    <form onSubmit={handleSubmite}>
+    <input 
+    type="text" 
+    value={expression} 
+    onChange={ e => setExpression(e.target.value)} 
+    placeholder="Digite " />
+    <input type="submit" value="Enviar" />
+    </form>
+    
+    <p>{result}</p>
+    
+    </Container>
+    );
+  }
+  
