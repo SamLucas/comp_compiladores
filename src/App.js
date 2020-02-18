133fac7645b9935854942efef99e9c8e79a7d55c
Samuel Lucas Santos Gomes <samuellucas0603@gmail.com>
2020-02-17 21:13:25 -0300

    Primeira versão do conversor.

--- src/App.js
+++ src/App.js
@@ -1,67 +1,52 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-import { Container } from './styles';
+import { Container } from "./styles";
 
 export default function App() {
-  
   const [result, setResult] = useState();
-  const [expression, setExpression] = useState('');
-  
-  const point = c => {
-    let pc = 0;
-    
-    if(c === '*' || c === '/')pc = 2;
-    else if(c === '+' || c === '-') pc = 1;
-    else pc = 4;
-    
-    return pc;
-  }
-  
+  const [expression, setExpression] = useState("1*3/3-6+7*7/3");
+
+  const Preferences = c => (c === "*" ? 3 : c === "/" ? 2 : 1);
+
+  const CompareBigger = (exp1, exp2) => Preferences(exp1) < Preferences(exp2);
+
   const handleSubmite = e => {
-    
     e.preventDefault();
-    
-    var result = '';
+
+    var result = "";
     var pilha = [];
-    
-    for(let k=0;k<expression.length;k++){
-      if(!isNaN(expression[k])) {
-        result += expression[k];
-      } else {
 
-        if(pilha.length() !== 0) pilha.push(expression[k]);
-        else if(point(pilha.top()) >=  point(expression[k])){
-          
-          let newCaracter = pilha.top();
-          pilha.pop();
-
-          result += newCaracter;
-          pilha.push(expression[k]);
-        } else result += expression[k];
+    for (let k = 0; k < expression.length; k++) {
+      if (!isNaN(expression[k])) result += expression[k];
+      else {
+        if (pilha.length === 0) pilha.unshift(expression[k]);
+        else if (CompareBigger(pilha[0], expression[k])) {
+          if (pilha.length > 0) pilha.unshift(expression[k]);
+          else result += expression[k];
+        } else {
+          result += pilha.shift();
+          pilha.unshift(expression[k]);
+        }
       }
     }
 
-    let newCaracter = pilha.top();
-    pilha.pop();
-    result += newCaracter;
+    result += pilha.shift();
     setResult(result);
+  };
 
-  }
-  
   return (
     <Container>
-    <form onSubmit={handleSubmite}>
-    <input 
-    type="text" 
-    value={expression} 
-    onChange={ e => setExpression(e.target.value)} 
-    placeholder="Digite " />
-    <input type="submit" value="Enviar" />
-    </form>
-    
-    <p>{result}</p>
-    
+      <form onSubmit={handleSubmite}>
+        <input
+          type="text"
+          value={expression}
+          onChange={e => setExpression(e.target.value)}
+          placeholder="Digite "
+        />
+        <input type="submit" value="Enviar" />
+      </form>
+
+      <p>{result}</p>
     </Container>
-    );
-  }
-  
+  );
+}
